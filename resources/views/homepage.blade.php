663db663e23101b76ffc2737ce4741cc61722fee
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-05-19 16:17:43 +0200

    homepage layout done

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <div class="container">
+
+            <h1>
+                Hey, I'm Francesco
+            </h1>
+
+            <p>A Full Stack Web Developer</p>
+
+            <button>
+                <a href="">PROJECTS</a>
+            </button>
+
+    </div>
+
+@endsection
